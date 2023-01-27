e7b8949d63079c5dc5624ae50804b90f324f84ff
Bhatia Parth <123570596+Parth6352@users.noreply.github.com>
2023-01-27 19:45:08 +0530

    Added Day1 first Project

---
+++ Day 1/day_1_first/src/main/java/com/htrap/day_1_first/Day1FirstApplication.java
@@ -0,0 +1,13 @@
+package com.htrap.day_1_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1FirstApplication.class, args);
+	}
+
+}
---
+++ Day 1/day_1_first/src/test/java/com/htrap/day_1_first/Day1FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htrap.day_1_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
